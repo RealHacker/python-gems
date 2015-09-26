294dcb6edd5d4a65ac8e981c25cfe47e3cd3778f
Neo <wonglei@gmail.com>
2015-09-26 14:44:35 +0800

    Implement proxy

--- http_server/web_server.py
+++ http_server/web_server.py
@@ -8,14 +8,15 @@ import shutil
 import json
 import sys
 import urllib
-
+import requests
+import copy
 try:
-    import CStringIO as StringIO
+    import cStringIO as StringIO
 except:
     import StringIO
 
 # templates
-error_tpl = """
+error_tpl = u"""
     <html>
         <head>
             <title>Error %d</title>
@@ -122,8 +123,12 @@ class WebServer(object):
                 except WSGIInvalid, WSGIFileNotFound:
                     handler = None
                     add_error_log("WSGI file invalid, ignoring path %s"%path)
+            elif d['type'] =="proxy":
+                handler = ProxyHandler(path, d['proxyurl'])
             else:
                 add_error_log("Unsupported path definition: %s"%path)
+            # link the handler and self
+            handler.server = self
             try:
                 mux.register_handler(path, handler)
             except DuplicatePath:
@@ -153,14 +158,11 @@ class StaticHandler(object):
     def handle_request(self, serv):
         if serv.verb.lower() != "get":
             serv.send_error_response(400, "Unsupported HTTP Method")
-        # get the relative path
-
+        # get the file system real path for the file/dir
         parsed = urlparse.urlparse(serv.path)
         relative_path = parsed.path[len(self.virtual_path):]
-        relative_path = urllib.unquote(relative_path)
-        relative_path = relative_path.strip()
-        real_path = os.path.join(self.static_dir, relative_path)
-        print real_path
+        unquoted_path = urllib.unquote(relative_path)
+        real_path = self.static_dir + unquoted_path.decode(sys.getfilesystemencoding())
        
         if not os.path.exists(real_path):
             serv.send_error_response(404, "File/directory not found.")
@@ -171,35 +173,49 @@ class StaticHandler(object):
             listing = os.listdir(real_path)
             if not relative_path.endswith("/"):
                 relative_path = relative_path + "/" 
-            # first try index.html
+
+            # first try index.html if exists
             index_files = ["index.html", "index.htm"]
             for index in index_files:
                 if index in listing:
                     serv.send_response_line(302, "Redirected")
-                    index_path = os.path.join(self.virtual_path+relative_path, index)
+                    index_path = os.path.join(self.virtual_path + relative_path, index)
                     serv.send_header("Location", index_path)
                     serv.end_headers()
                     return
+
+            # index.html not present, generate the listing html
             # Send the response line and headers
             serv.send_response_line(200, "OK") 
             serv.send_header("Content-Type", "text/html;charset=%s"%sys.getfilesystemencoding())
             serv.send_header("Connection", "close")
-            # index.html not present, generate the listing html
+            # Construct HTML
             listing_str = ""
             if relative_path != "/":
                 # if not root, add parent directory link
                 parent_path = "/".join(relative_path.split("/")[:-2])
-                href = os.path.normpath(self.virtual_path + parent_path)
-                href = urllib.quote(href)
+                href = self.virtual_path + parent_path
                 line = u"<a href='%s'>..</a><br>"%href
                 listing_str += line
+            # construct the file list
             for item in listing:
-                href = os.path.join(self.virtual_path+relative_path, item)
-                snippet = "<a href='%s'>%s</a><br>"%(href, item)
+                if isinstance(item, unicode):
+                    # decode to unicode
+                    stritem = item.encode(sys.getfilesystemencoding())
+                    unicodeitem = item
+                else:
+                    stritem = item
+                    unicodeitem = item.decode(sys.getfilesystemencoding())
+                # urllib.quote must be given str, not unicode
+                quoted_item = urllib.quote(stritem)
+                # construct url with quoted file name
+                href = os.path.join(self.virtual_path+relative_path, quoted_item)
+                snippet = u"<a href='%s'>%s</a><br>"%(href, unicodeitem)
                 listing_str += snippet
+
             display_path = self.virtual_path+relative_path
             listing_html = listing_tpl%(display_path, display_path, listing_str)
-            listing_html = listing_html.encode("utf-8")
+            listing_html = listing_html.encode(sys.getfilesystemencoding())
             serv.send_header("Content-Length", len(listing_html))
             serv.end_headers() 
             serv.wfile.write(listing_html)
@@ -211,6 +227,8 @@ class StaticHandler(object):
                 return
             serv.send_response_line(200, "OK") 
             _, ext = os.path.splitext(real_path)
+            # ignore case of extension
+            ext = ext.lower() 
             # make a guess based on mimetypes
             content_type = mimetypes.types_map.get(ext, '')
             if not content_type:
@@ -226,6 +244,54 @@ class StaticHandler(object):
             # now copy the file over
             shutil.copyfileobj(f, serv.wfile)
 
+class ProxyHandler(object):
+    def __init__(self, virtual_path, proxyurl):
+        self.virtual_path = virtual_path
+        if proxyurl.endswith("/"):
+            self.proxyurl = proxyurl[:-1]
+        else:
+            self.proxyurl = proxyurl
+
+    def handle_request(self, serv):
+        parsed = urlparse.urlparse(serv.path)
+        real_path = parsed.path[len(self.virtual_path):]
+        if not real_path.startswith("/"):
+            real_path= "/" + real_path
+        real_url = self.proxyurl + real_path
+
+        headers = copy.copy(serv.headers)
+        del headers["Cookie"]
+        headers['Host'] = urlparse.urlparse(self.proxyurl).netloc
+
+        # Read the body of request
+        body = ""
+        if 'Content-Length' in headers and headers["Content-Length"]:
+            body = serv.rfile.read(int(headers["Content-Length"]))
+        print body
+        # use requests to do the dirty work
+        try:
+            response = requests.request(serv.verb.upper(), real_url, 
+                params=parsed.query, headers=headers, data=body,
+                allow_redirects=False)
+        except Exception, e:
+            add_error_log("Error when sending request to proxyurl:"+ str(e))
+            import traceback
+            print traceback.format_exc()
+            raise e
+        # just translate
+        serv.send_response_line(response.status_code, response.reason)
+        for header in response.headers:
+            # encoding has been handled by requests
+            if header.lower()=="content-encoding" or header.lower()=="transfer-encoding":
+                continue
+            serv.send_header(header, response.headers[header])
+        serv.send_header("Content-Length", len(response.content))
+
+        serv.end_headers()
+        
+        serv.wfile.write(response.content)
+
+
 class WSGIHandler(object):
     def __init__(self, virtual_path, app_path):
         self.virtual_path = virtual_path
@@ -234,20 +300,19 @@ class WSGIHandler(object):
     def load_application(self, app_path):
         if not os.path.exists(app_path):
             raise WSGIFileNotFound()
-        filename = os.path.split(app_path)[-1]
-        modulename, ext = os.path.splitext(app_path)
-        print modulename
+        path, filename = os.path.split(app_path)
+        modulename, ext = os.path.splitext(filename)
+        # Add the path to sys.path
+        sys.path.append(path)
         try:
-            # if ext.lower() == ".py" or ext.lower() == ".wsgi":
-            #     m = imp.load_source(modulename, app_path)
-            # else:
-            #     m = imp.load_compiled(modulename, app_path)
-            __import__(modulename)
+            if ext.lower() == ".py" or ext.lower() == ".wsgi":
+                m = imp.load_source(modulename, app_path)
+            else:
+                m = imp.load_compiled(modulename, app_path)
         except Exception as e:
             add_error_log(str(e))
             raise WSGIInvalid()
         else:
-            m = sys.modules[modulename]
             if not hasattr(m, "application"):
                 add_error_log("Wsgi application not found")
                 raise WSGIInvalid()
@@ -267,17 +332,17 @@ class WSGIHandler(object):
                 headers_environ[key] = val
             else:
                 headers_environ[key] += "," + val
-        return header_environ
+        return headers_environ
     
     def prepare_environ(self, serv):
         parsed = urlparse.urlparse(serv.path)
         real_path = parsed.path[len(self.virtual_path):]
         if not real_path.startswith("/"):
-            real_path= "/" + realpath
+            real_path= "/" + real_path
         environ = {
             "REQUEST_METHOD":   serv.verb,
             "SCRIPT_NAME":      self.virtual_path,
-            "PATH_INFO":        realpath,
+            "PATH_INFO":        real_path,
             "QUERY_STRING":     parsed.query,
             "CONTENT_TYPE":     serv.headers.get("Content-Type", ""),
             "CONTENT_LENGTH":   serv.headers.get("Content-Length", ""),
@@ -289,8 +354,8 @@ class WSGIHandler(object):
             "wsgi.version":     (1,0),
             "wsgi.run_once":    False,
             "wsgi.url_scheme":  "http", 
-            "wsgi.multithread": True,
-            "wsgi.multiprocess": False, 
+            "wsgi.multithread":     self.server._mode=="thread",
+            "wsgi.multiprocess":    self.server._mode=="process", 
         }
         environ.update(self.get_headers_environ(serv))
         return environ
@@ -305,9 +370,14 @@ class WSGIHandler(object):
                 serv.send_header(k, v)
             serv.end_headers()
         # Get response lines
-        response_chucks = self.app(environ, start_response)
-        for chuck in response_chucks:
-            serv.wfile.write(chuck)
+        try:
+            response_chucks = self.app(environ, start_response)
+        except Exception as e:
+            add_error_log("* ERROR IN WSGI APP *" + str(e))
+            raise e
+        else:
+            for chuck in response_chucks:
+                serv.wfile.write(chuck)
 
 # This is the handler entry point, dispatching requests to different handlers with the help of mux
 class HTTPServerHandler(SocketServer.StreamRequestHandler):
@@ -318,40 +388,45 @@ class HTTPServerHandler(SocketServer.StreamRequestHandler):
     # Should read the request from self.rfile
     # and write the response to self.wfile
     def handle_one_request(self):
-        # try:
-        # read the first line from request
-        request_line = self.rfile.readline()
-        words = request_line.strip().split()
-        if len(words) != 3:
-            self.send_error_response(400, "Invalid HTTP request")
-            return
-        self.verb, self.path, _ = words
-        print self.verb, self.path
-        # read the header lines
-        self.headers = mimetools.Message(self.rfile, 0)
-        print self.headers
-        connection_type = self.headers.get("Connection", "")
-        if connection_type == "close":
+        try:
+            # read the first line from request
+            request_line = self.rfile.readline()
+            words = request_line.strip().split()
+            if len(words) != 3:
+                self.send_error_response(400, "Invalid HTTP request")
+                return
+            self.verb, self.path, _ = words
+            add_access_log(self.verb+" "+self.path)
+            # read the header lines
+            self.headers = mimetools.Message(self.rfile, 0)
+            connection_type = self.headers.get("Connection", "")
+            if connection_type == "close":
+                self.close_connection = True
+            elif connection_type == "keep-alive":
+                self.close_connection = False
+
+            # delegate body handling to mux
+            handler = mux.get_handler(self.path)
+            if not handler:
+                self.send_error_response(404, "File Not Found")
+                return
+            handler.handle_request(self)
+            if not self.wfile.closed:
+                self.wfile.flush()
+                self.wfile.close()
+
+            # If the request handler write some error messages, record them in log
+            if self.error.tell():
+                self.error.seek(0)
+                add_error_log(self.error.read())
+        except Exception, e:
+            add_error_log(str(e))
             self.close_connection = True
-        elif connection_type == "keep-alive":
-            self.close_connection = False
-        # delegate body handling to mux
-        handler = mux.get_handler(self.path)
-        if not handler:
-            self.send_error_response(404, "File Not Found")
-            return
-        handler.handle_request(self)
-        if not self.wfile.closed:
-            self.wfile.flush()
-        if self.error.len:
-            add_error_log(self.error.read())
-        # except Exception, e:
-        #     add_error_log(str(e))
-        #     self.close_connection = True
                         
     def handle(self):
         self.close_connection = True
         self.handle_one_request()
+        # supporting keep-alive
         while not self.close_connection:
             self.handle_one_request()
 
@@ -402,6 +477,9 @@ def timestamp_to_string(timestamp=None):
 
 def add_error_log(entry):
     print "[ERROR] - " + entry
+
+def add_access_log(entry):
+    print "[REQUEST] - " + entry
 
 # The driver
 def main():
